b2a6fa87276add4328c1a50cdcbea9abe7d04c8a
BimTheProgrammuh <bimtomtv@gmail.com>
2025-04-16 21:35:30 +0700

    Working progressbar, bug: not accepting 1 and 2 as input

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+const div1 = document.getElementById("progress-div-1")
+const div2 = document.getElementById("progress-div-2")
+const div3 = document.getElementById("progress-div-3")
+
+progressBars = [div1,div2,div3]
+
+tasks = {
+    "placeholder1":1,
+    "placeholder2":24,
+    "placeholder3":8760,
+}
+taskList = Object.keys(tasks)
+
+let valid = false
 document.getElementById("form").addEventListener("submit",function(e){
     e.preventDefault()
+
+    resetProgressBars()
+    alert("Bar reset!")
 
     const form = e.target;
     const hours = form.hours.value;
     const label = document.getElementById("status")
+    alert("Form result got! Value: "+hours)
     if(!isNaN(hours) && hours.trim() !==""){
         label.textContent = `Here is what you can achieve with your ${hours} hours per day productivity!`
+        updateProgressBars(parseFloat(hours))
     }
     else if(typeof hours === "string"){
         label.textContent = "U can't even type a number? Can't be useful!"
@@ -13,6 +32,18 @@ document.getElementById("form").addEventListener("submit",function(e){
     else{
         label.textContent = "What is even that data type..."
     }
+})  
 
-    
-})
+function updateProgressBars(hours) {
+    for(let i=0;i<progressBars.length;i+=1){
+        taskKey = taskList[i]
+        progress = Math.min((hours/tasks[taskKey])*100,100)
+        progressBars[i].style.width = progress + "%"
+    }
+}
+
+function resetProgressBars() {
+    progressBars.forEach(bar => {
+        bar.style.width = '0%';
+    });
+}
